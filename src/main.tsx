10c78ba8f866d17bc679927b60ebf3fe01bdfdc1
anujkanojia29 <anujkanojia@gmail.com>
2025-10-03 22:39:43 +0530

    handled vite env

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { PostHogProvider } from 'posthog-js/react'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { PostHogProvider } from "posthog-js/react";
+import App from "./App.tsx";
+import "./index.css";
 
+const posthogKey = import.meta.env.VITE_POSTHOG_KEY;
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <PostHogProvider apiKey={process.env.VITE_PUBLIC_POSTHOG_KEY || ''}>
+    <PostHogProvider apiKey={posthogKey}>
       <App />
     </PostHogProvider>
-  </StrictMode>,
+  </StrictMode>
 );
